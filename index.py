88df510905c5dc13e00d36514b17e29c565dd4bb
csamatov96 <996bkg@gmail.com>
2019-08-20 21:01:19 -0500

    index.py added

---
+++ index.py
@@ -0,0 +1,14 @@
+from flask import Flask #
+app = Flask(__name__) #
+@app.route("/") #
+def hello(): #
+    return "Hello World!" #
+@app.route("/home") #
+def hello(): #
+    return "Hello World again!" #
+@app.route("/user") #
+def hello(): #
+    return "Hello World, Hello!" #
+
+if __name__ == "__main__": #
+    app.run(host="0.0.0.0", port=int("5000"), debug=True) #
